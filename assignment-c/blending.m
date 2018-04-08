2367be2fced43d90f93e281ca93205cba5c10886
Michail Vrachasotakis <mikvrax@gmail.com>
2018-04-09 00:51:39 +0300

    Submitted versions of Task 1 and 2

---
+++ assignment-c/blending.m
@@ -0,0 +1,62 @@
+function [] = blending(source_filename, target_filename, x, y, a)
+% target image
+image=imread(target_filename);
+[h_target w_target d_target]=size(image);
+U = double(reshape(image,w_target*h_target,d_target))/255;
+
+% source image
+image_source=imread(source_filename);
+[h_source w_source d_source]=size(image_source);
+U_source = double(reshape(image_source,w_source*h_source,d_source))/255;
+
+% extended (padded) source image
+extended = padarray(image_source, [1 1], 0);
+[h_extended w_extended d_extended]=size(extended);
+U_extended = double(reshape(extended,w_extended*h_extended,d_extended))/255;
+
+figure, imshow(extended);
+
+%% Create target box, later used to extract U_b
+
+% xy ranges in target image
+target_x_from = x - 1;
+target_x_to = x + w_source;
+target_y_from = y - 1;
+target_y_to = y + h_source;
+
+% create U vector containing all pixels from the target in size of the
+% source + 2
+targetbox = image(target_y_from:target_y_to, target_x_from:target_x_to,:);
+[h_targetbox w_targetbox d_targetbox]=size(targetbox);
+U_targetbox = double(reshape(targetbox,w_targetbox*h_targetbox,d_targetbox))/255;
+figure, imshow(targetbox);
+
+%inner and boundary gradients only
+G = gradient(h_extended, w_extended);
+
+%selector matrix, boundary pixels only
+S = selmat_rect(h_extended, w_extended);
+
+%gradients of source image
+g = G * U_extended;
+
+%set boundary gradients to zero
+g = boundary_gradients_zero(g, h_extended, w_extended);
+
+% make vector of boundary pixels
+U_b = S * U_targetbox;
+
+% linear system
+leftside =  G' * G + a *  (S'*S);
+rightside = G'*g + a*S'*U_b;
+U_blended = leftside\rightside;
+
+% blended image
+image_blended =uint8(reshape(U_blended,h_extended,w_extended,d_source)*255);
+figure, imshow(image_blended)
+
+% blended image pasted in the target image
+image_final = image;
+image_final(y:y+h_extended-1,x:x+w_extended-1,:) = image_blended;
+figure, imshow(image_final)
+end
--- assignment-c/sharpening.m
+++ assignment-c/sharpening.m
@@ -1,26 +1,35 @@
 function [] =  sharpening(input_filename, output_filename, cs, cu)
 
+%read and rescale input image
 image=imread(input_filename);
 [h w d]=size(image);
 U = double(reshape(image,w*h,d))/255;
-cs = cs;
-cu = cu;
-G = gradient2(h,w);
+
+%create gradient matrix G
+G = gradient(h,w);
+
+%compute gradients of input image
 g = G * U;
 
+%compute right and left hand size of linear system
 L = G'*G;
 rightside = cs*G'*g + cu*U;
 leftside =  L + cu*speye(size(L));
 
+%solve linear system
 U2 = leftside\rightside;
 
+%display original image
 image =uint8(reshape(U,h,w,d)*255);
 figure
 subplot(2,1,1), imshow(image)
 title('Original Image')
 
+%display sharpened image
 image2 =uint8(reshape(U2,h,w,d)*255);
 subplot(2,1,2), imshow(image2)
 title('Sharpened Image')
 
+%save sharpened image to file
 imwrite(image2,output_filename)
+end
